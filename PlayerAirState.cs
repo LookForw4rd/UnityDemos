950489c3c10b5ba8bc28827b48a71f63c1dd1627
MakiseKurisu <320358301@qq.com>
2025-06-18 23:43:28 +0800

    move jump air states created

---
+++ PlayerAirState.cs
@@ -0,0 +1,24 @@
+public class PlayerAirState : PlayerState
+{
+    public PlayerAirState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        
+        if (rb.linearVelocity.y == 0)
+            stateMachine.ChangeState(player.idleState);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}
